5d34b4fcc2f7760e0df0f665244a6d321368df36
Pranav Patnaik <137661401+pranavpatnaik1@users.noreply.github.com>
2026-09-07 22:44:22 -0400

    Add: design-word-search-data-structure - submission-25

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-25.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            
+            curr = curr.children[c]
+        
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def searchHelper(root, pos: int):
+            if pos == len(word):
+                return root.endOfWord
+            
+            if word[pos] in root.children:
+                return searchHelper(root.children[word[pos]], pos + 1)
+            
+            if word[pos] == ".":
+                for c in root.children:
+                    if searchHelper(root.children[c], pos + 1):
+                        return True
+                
+                return False
+            
+            return False
+        
+        return searchHelper(self.root, 0)
+
+    
+
