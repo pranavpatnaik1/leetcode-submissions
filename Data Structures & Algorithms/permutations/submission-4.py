fc1674033aa6ead2f1e773602325c53710811bd1
Pranav Patnaik <137661401+pranavpatnaik1@users.noreply.github.com>
2026-09-08 22:16:11 -0400

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def findPermutations(currList, resList):
+            if len(currList) == len(nums):
+                res.append(currList)
+                return
+            
+            lenRes = len(resList)
+            for i in range(lenRes):
+                findPermutations(currList + [resList[i]], resList[:i] + resList[i+1:])
+            
+        
+        findPermutations([], nums)
+        return res
