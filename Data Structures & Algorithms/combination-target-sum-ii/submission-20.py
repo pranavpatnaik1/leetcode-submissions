45dae145cc094e667feab99cbf388f0cd627ecf8
Pranav Patnaik <137661401+pranavpatnaik1@users.noreply.github.com>
2026-09-08 20:56:37 -0400

    Add: combination-target-sum-ii - submission-20

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-20.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def findCombination(currList, pos, remaining):
+            if remaining == 0:
+                res.append(currList.copy())
+                return
+            
+            if remaining < 0 or pos >= len(candidates) or candidates[pos] > remaining:
+                return
+            
+            next_pos = pos + 1
+            while next_pos < len(candidates) and candidates[next_pos] == candidates[pos]:
+                next_pos += 1
+            
+
+            findCombination(currList + [candidates[pos]], pos + 1, remaining - candidates[pos])
+            findCombination(currList, next_pos, remaining)
+        
+        findCombination([], 0, target)
+
+        return res
