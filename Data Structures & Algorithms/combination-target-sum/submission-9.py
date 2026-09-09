744c06a87d1404dac0d8c5f73f5fde35730820f5
Pranav Patnaik <137661401+pranavpatnaik1@users.noreply.github.com>
2026-09-08 21:53:37 -0400

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def findCombination(currList, pos, remaining):
+            if remaining == 0:
+                res.append(currList.copy())
+                return
+            
+            if pos >= len(nums) or remaining < 0:
+                return
+            
+            findCombination(currList + [nums[pos]], pos, remaining - nums[pos])
+            findCombination(currList, pos + 1, remaining)
+        
+        findCombination([], 0, target)
+        return res
