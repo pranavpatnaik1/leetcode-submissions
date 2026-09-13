84bd8ece48d844b803993cd3edcde6d42bf77209
Pranav Patnaik <137661401+pranavpatnaik1@users.noreply.github.com>
2026-09-13 01:54:30 -0400

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = dict()
+
+        def checkCost(pos):
+            if pos >= len(cost):
+                return 0
+            
+            if pos in memo:
+                return memo[pos]
+            
+            take = checkCost(pos + 1)
+            skip = checkCost(pos + 2)
+
+            memo[pos] = cost[pos] + min(take, skip)
+            return memo[pos]
+        
+        return min(checkCost(0), checkCost(1))
